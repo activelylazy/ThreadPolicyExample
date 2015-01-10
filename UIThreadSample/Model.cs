262f74758c752b9b1591214cbb9bd53ac2f7f7c9
David Green <dave@activelylazy.co.uk>
2015-01-10 17:26:38 +0100

    Initial version

---
+++ UIThreadSample/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UIThreadSample
+{
+    class Model
+    {
+        public async Task<IList<string>> Fetch()
+        {
+            return await Task.Run(() => DoFetch());
+        }
+
+        private IList<string> DoFetch()
+        {
+            Thread.Sleep(1000);
+            return new[] { "Hello" };
+        }
+    }
+}
---
+++ UIThreadSample/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UIThreadSample
+{
+    class ViewModel
+    {
+        private Model m_model = new Model();
+
+        public ObservableCollection<string> Items { get; private set; }
+        public ICommand FetchCommand { get; private set; }
+
+        public ViewModel()
+        {
+            Items = new ObservableCollection<string>();
+            FetchCommand = new FetchCommand(this);
+        }
+
+        public async void Fetch()
+        {
+            var items = await m_model.Fetch();
+            foreach (var item in items)
+                Items.Add(item);
+        }
+    }
+
+    class FetchCommand : ICommand
+    {
+        private ViewModel m_viewModel;
+
+        public FetchCommand(ViewModel viewModel)
+        {
+            m_viewModel = viewModel;
+        }
+
+        public void Execute(object param)
+        {
+            m_viewModel.Fetch();
+        }
+
+        public bool CanExecute(object param)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+    }
+}
